2473dc88eca15feffd0ea1e501d2e0bdf057c7b5
Tanveer Ahmed <tanveer@test.com>
2026-02-28 14:57:55 +0530

    basic text generation

---
+++ text-generation-app/main.py
@@ -0,0 +1,62 @@
+from urllib import response
+
+import tiktoken
+from openai import OpenAI
+
+client = OpenAI(
+  base_url="http://localhost:1234/v1",
+  api_key="lm-studio"
+)
+MODEL = "google/gemma-3-4b"
+
+def get_completion(prompt):
+  messages = [{"role": "user", "content": prompt}]
+
+  response = client.chat.completions.create(
+    model=MODEL,
+    messages=messages,
+    temperature=0.1,
+    max_tokens=1000,
+    stream=True
+  )
+
+  full_response = []
+  for chunk in response:
+    delta = chunk.choices[0].delta.content
+    if delta:
+      print(delta, end="", flush=True)
+      full_response.append(delta)
+
+  print()
+  return "".join(full_response)
+
+def main1():
+  prompt ="Show me 5 recipes for a dish with the following ingredients: chicken, potatoes, and carrots. Per recipe, list all the ingredients used"
+
+  response = get_completion(prompt)
+  print(response)
+
+def main2():
+  total_recipies = input("How many recipes do you want to see? ")
+  ingredients = input("Enter list of ingredients? ")
+  filter = input("List of ingredients to exclude? ")
+
+  prompt = f"Show me {total_recipies} recipes for a dish with the following ingredients: {ingredients}. Per recipe, list all the ingredients used. Do not include any recipes that use ingredients from this list: {filter}. Just return the recipes, do not include any other text."
+
+  recipies = get_completion(prompt)
+
+  print("Shopping List:")
+  
+  existing_ingredients = input("Enter list of ingredients you already have? ")
+
+  prompt = f"Produce shopping list for the above recipes. Group the ingredients by recipe.Do not include the ingredient that I already have: {existing_ingredients}. Do not include any other text."
+  
+  new_prompt = f"{recipies} {prompt}"
+
+  get_completion(new_prompt)
+
+
+
+
+if __name__ == "__main__":
+  main2()
